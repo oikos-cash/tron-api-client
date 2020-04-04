d74ca522bebbcbfdff1f1937bff9bad4e1c57f7c
Kevin Holder <kevholderx@gmail.com>
2020-04-04 18:00:12 -0400

    fix ssl issue?

--- ci/install.sh
+++ ci/install.sh
@@ -31,11 +31,14 @@ main() {
     esac
 
     # This fetches latest stable release
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
-                       | cut -d/ -f3 \
-                       | grep -E '^v[0.1.0-9.]+$' \
-                       | $sort --version-sort \
-                       | tail -n1)
+    # local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+    #                    | cut -d/ -f3 \
+    #                    | grep -E '^v[0.1.0-9.]+$' \
+    #                    | $sort --version-sort \
+    #                    | tail -n1)
+		# TODO: this is a temp fix to be able to use openssl...
+		# https://github.com/rust-embedded/cross/issues/229#issuecomment-591084850
+		local tag="v0.1.16"
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
